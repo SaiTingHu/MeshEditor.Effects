ee639c19a70c8c7b11e50b67bbae21fe1b1c58c9
SaiTingHu <987947865@qq.com>
2020-04-13 18:06:11 +0800

    新增：网格变形特效。

---
+++ Editor/MeshTransformer/MeshTransformerInspector.cs
@@ -0,0 +1,21 @@
+﻿using UnityEditor;
+
+namespace MeshEditor.Effects
+{
+    [CustomEditor(typeof(MeshTransformer))]
+    internal sealed class MeshTransformerInspector : MeshEffectsBaseInspector<MeshTransformer>
+    {
+        protected override string HeaderName
+        {
+            get
+            {
+                return "MESH TRANSFORMER";
+            }
+        }
+
+        protected override void OnMeshEffectsGUI()
+        {
+            base.OnMeshEffectsGUI();
+        }
+    }
+}
---
+++ RunTime/MeshTransformer/MeshTransformer.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using DG.Tweening;
+
+namespace MeshEditor.Effects
+{
+    /// <summary>
+    /// 网格变形特效
+    /// </summary>
+    [DisallowMultipleComponent]
+    public class MeshTransformer : MeshEffectsBase
+    {
+        protected override void BeginEffect(MeshData meshData)
+        {
+            
+        }
+
+        protected override void UpdateEffect(MeshData meshData)
+        {
+            
+        }
+    }
+}
